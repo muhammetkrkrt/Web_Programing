9a44b0d796c6289fb4018d6aed8771329aa1ca6c
Muhammet Ali KARAKURT <karakurt3656@gmail.com>
2023-06-01 13:37:08 +0300

    Web

---
+++ JavaScrip_WorkSpace/example_00/app.js
@@ -0,0 +1,15 @@
+let sayi = 2 ;
+const sayi2 = 5;
+
+sayi2 =sayi + 5 ;
+console.log(sayi2);
+
+let sayilar = [1,2,6,9,8,22,4,2,5];
+
+console.log(sayilar);
+
+
+let sayilar2 = new Array();
+console.log(sayilar2);
+
+
---
+++ JavaScrip_WorkSpace/example_00/app2.js
@@ -0,0 +1,9 @@
+
+function  toplamHarcama(user) {
+    let toplam = 0 ;
+
+    user.expenses.fo
+}
+
+var email =prompt("email?");
+var sifre = prompt("Sifreniz?");
